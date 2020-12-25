1f217b8b8895d97d1857154435c107d760a20790
alexzarp <alexszds@gmail.com>
2020-12-25 15:29:24 -0300

    Novo

--- curso_flutter/projeto_perguntas/lib/main.dart
+++ curso_flutter/projeto_perguntas/lib/main.dart
@@ -21,21 +21,17 @@ class _PerguntaAppState extends State<PerguntaApp> {
 		final List<Map<String, Object>> perguntas = [
 
 			{ 'texto': 'Qual é a sua cor favorita?',
-        'respostas': ['Preto','Vermelho','Verde','Branco'] },
+        'respostas': ['Preto','Vermelho','Verde','Branco'], },
 
       { 'texto': 'Qual é o seu animal preferido?',
-        'respostas': ['Coelho','Cobra','Elefante','Leão'] },
+        'respostas': ['Coelho','Cobra','Elefante','Leão'], },
 
       { 'texto': 'Qual é o seu time?',
-        'respostas': ['TeamOferflow','TimeB','TeamStupid','EquipeGuarda'] },
+        'respostas': ['TeamOferflow','TimeB','TeamStupid','EquipeGuarda'], },
 		];
 
-    List<Widget> respostas = [];
-
-    for (String textoResp in perguntas[_perguntaSelecionada] ['repostas']) {
-      respostas.add(Resposta(textoResp, _responder));
-    }
-
+    List<String> respostas = perguntas[_perguntaSelecionada] ['repostas'];
+      
 		return MaterialApp(
 			home: Scaffold(
 				appBar: AppBar(
@@ -44,7 +40,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
 				body: Column(
 					children: <Widget>[
 						Questao(perguntas[_perguntaSelecionada] ['texto']),
-            ...respostas,
+            ...respostas.map((t) => Resposta(t, _responder)).toList(),
 					],
 				),
 			),
