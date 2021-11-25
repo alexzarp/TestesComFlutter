79c749e2efa2095bf23a2b1b5654475b3e2a03a8
alexzarp <alexszds@gmail.com>
2021-11-25 20:49:09 -0300

    update

---
+++ login_app/lib/pages/login.page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 128,
+              height: 128,
+              child: Image.asset("assets/logo.png"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
